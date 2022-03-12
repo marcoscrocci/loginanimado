4f6dc900032ade272e0f7b29bb2bf0112346da87
marcoscrocci <marcoscrocci@gmail.com>
2022-03-12 20:04:39 -0300

    Projeto finalizado.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,100 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import * as Animatable from "react-native-animatable";
+import { useNavigation } from "@react-navigation/native";
 
 export default function SignIn() {
+
+    const navigation = useNavigation();
+    
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+                <Text style={styles.message}>Bem-vindo(a)</Text>
+            </Animatable.View>
+            <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+                <Text style={styles.title}>Email</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite seu email..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+                <Text style={styles.title}>Senha</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite sua senha..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Entrar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonRegister}>
+                    <Text style={styles.registerText}>Não possui uma conta? Cadastre-se</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonRegister} onPress={() => navigation.goBack()}>
+                    <Text style={styles.registerText}>Voltar</Text>
+                </TouchableOpacity>
+            </Animatable.View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#38a69d',
+    },
+    containerHeader: {
+        marginTop: '14%',
+        marginBottom: '8%',
+        paddingStart: '5%'
+    },
+    message: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#FFF'
+    },
+    containerForm: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+    title: {
+        fontSize: 20,
+        marginTop: 28
+    },
+    input: {
+        borderBottomWidth: 1,
+        height: 40,
+        fontSize: 16,
+        marginBottom: 12
+    },
+    button: {
+        backgroundColor: '#38a69d',
+        width: '100%',
+        borderRadius: 4,
+        paddingVertical: 8,
+        marginTop: 14,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    buttonRegister: {
+        marginTop: 14,
+        alignSelf: 'center'
+    },
+    registerText: {
+        color: '#a1a1a1'
+    }
+})
